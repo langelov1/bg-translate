85bb49f50c9b0d2fb3c265177ad5626ecb8348b8
Lachezar Angelov <lachezar.angelov@domgrid.com>
2026-03-22 13:54:44 +0000

    Fix build: update pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,2 @@
 from setuptools import setup, find_packages
-
-setup(
-    name="bg_translations",
-    version="0.0.1",
-    description="Bulgarian translations for ERPNext",
-    author="Lachezar Angelov",
-    author_email="lachezar.angelov@domgrid.com",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-)
+setup(name="bg_translations", version="0.0.1", packages=find_packages())
